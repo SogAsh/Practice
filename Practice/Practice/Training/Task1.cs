008b6686e97d5085500d276467a39456e2e7cb6d
SogAsh <sogdian4815@mail.ru>
2022-03-28 23:17:29 +0500

    hjhj

--- Practice/Practice/Training/Task1.cs
+++ Practice/Practice/Training/Task1.cs
@@ -14,17 +14,21 @@ namespace Practice.Training
      */
     public class Task1
     {
-        public static Array CountElements()
+        public static void CountElements()
         {
+            Console.WriteLine("Введите длину масива");
             var countElements = Console.ReadLine();
 
             var array = new int[int.Parse(countElements)];
 
             int elements;
             int coint = 0;
+            int cointForResultArray = 0;
+
 
             for (int i = 0; i < array.Length; i++)
             {
+                Console.WriteLine("Введите элемент масива");
                 elements = int.Parse(Console.ReadLine());
 
                 array[i] = elements;
@@ -33,15 +37,28 @@ namespace Practice.Training
                     coint++;
             }
 
-            var arr2 = new int[coint];
+            var resultArray = new int[coint];
 
             for (int i = 0; i < array.Length; i++)
             {
                 if (ChechEven(array[i]))
-                    arr2[i] = array[i];
+                {
+                    resultArray[cointForResultArray] = array[i];
+                    cointForResultArray++;
+                }
             }
 
-            return array;
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                Console.WriteLine("Элемент массива: " + resultArray[i]);
+            }
+
+            var finishArray = SwapMaxMin(resultArray);
+
+            //for (int i = 0; i < finishArray.Length; i++)
+            //{
+            //    Console.WriteLine("Элемент массива: " + finishArray[i]);
+            //}
         }
 
         public static bool ChechEven(int a)
@@ -49,6 +66,19 @@ namespace Practice.Training
             if (a % 2 == 0)
                 return true;
             return false;
+        }
+
+        public static int[] SwapMaxMin(int[] array)
+        {
+            var min = int.MaxValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+            }
+
+            return array;
         }
     }
 }
